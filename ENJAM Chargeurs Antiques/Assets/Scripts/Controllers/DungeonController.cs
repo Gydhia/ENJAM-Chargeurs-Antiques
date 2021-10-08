adb823279346e0d19bc44fc752e24a3c8663ad70
Killian ML <killian.motel@gmail.com>
2021-10-08 20:39:09 +0200

    killian: structures controller

--- ENJAM Chargeurs Antiques/Assets/Scripts/Controllers/DungeonController.cs
+++ ENJAM Chargeurs Antiques/Assets/Scripts/Controllers/DungeonController.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class DungeonController : MonoBehaviour
 {
     public int nbOfFloors = 3;
     public int floorSqrSize = 4;
-    public int nbOfEnemiesRoom = 8;
-    public int nbOfRewardRoom = 2;
+
+    public int EnemiesRooms = 8;
+    public int RemainingEnemiesRooms;
+
+    public int BRewardRooms = 1;
+    public int RemainingBRewardRooms;
+    public int ARewardRooms = 1;
+    public int RemainingARewardRooms;
 
     public static readonly Vector3[] Directions = { Vector3.up, Vector3.right, Vector3.down, Vector3.left };
 
@@ -28,8 +35,12 @@ public class DungeonController : MonoBehaviour
         public int y;
     }
 
-    private void Start()
+    public void Start()
     {
+        RemainingEnemiesRooms = EnemiesRooms;
+        RemainingARewardRooms = ARewardRooms;
+        RemainingBRewardRooms = BRewardRooms;
+
         Rooms = new Room[floorSqrSize, floorSqrSize];
 
         Location spawnLoc = GetBaseRoom(RoomType.Spawn);
@@ -38,7 +49,21 @@ public class DungeonController : MonoBehaviour
         Location bossLoc = GetBaseRoom(RoomType.Boss);
         Rooms[bossLoc.x, bossLoc.y] = new Room(RoomType.Boss);
 
+        GenerateNextRoom(spawnLoc);
 
+        for (int i = 0; i < RemainingARewardRooms + RemainingBRewardRooms + RemainingEnemiesRooms; i++)
+        {
+
+        }
+
+        // To randomize rooms
+        for (int i = 0; i < floorSqrSize; i++)
+        {
+            for (int j = 0; j < floorSqrSize; j++)
+            {
+
+            }
+        }
     }
 
     public Location GetBaseRoom(RoomType type)
@@ -57,11 +82,21 @@ public class DungeonController : MonoBehaviour
     public bool GenerateNextRoom(Location location)
     {
         List<Location> avLocation = GetAvailableDirections(location);
+
+        if(avLocation.Any(room => Rooms[room.x, room.y].Type == RoomType.Boss)) {
+            return false;
+        }
+
         Location? nextLocation = null;
         if (avLocation.Count > 0)
             nextLocation = avLocation[Random.Range(0, avLocation.Count)];
 
-        
+        RoomType? nextType = GetAvailableType();
+        if (!nextType.HasValue) {
+            return false;
+        }
+
+        Rooms[nextLocation.Value.x, nextLocation.Value.y] = new Room(nextType.Value);
 
         if (nextLocation != null)
              return GenerateNextRoom(nextLocation.Value);
@@ -71,22 +106,54 @@ public class DungeonController : MonoBehaviour
 
     public void RandomiseRoom(ref Room room)
     {
-
+        
     }
 
     public List<Location> GetAvailableDirections(Location loc, bool includeBtm = false)
     {
         List<Location> availableDir = new List<Location>();
 
-        if (loc.y + 1 < floorSqrSize && Rooms[loc.x + 1, loc.y] == null)
+        if (loc.y + 1 < floorSqrSize && Rooms[loc.x + 1, loc.y] == null || Rooms[loc.x + 1, loc.y].Type == RoomType.Boss)
             availableDir.Add(new Location(loc.x + 1, loc.y));
-        if (loc.y - 1 >= 0 && Rooms[loc.x - 1, loc.y] == null)
+
+        if (loc.y - 1 >= 0 && Rooms[loc.x - 1, loc.y] == null || Rooms[loc.x + 1, loc.y].Type == RoomType.Boss)
             availableDir.Add(new Location(loc.x - 1, loc.y));
-        if (loc.y + 1 < floorSqrSize && Rooms[loc.x, loc.y + 1] == null)
+
+        if (loc.y + 1 < floorSqrSize && Rooms[loc.x, loc.y + 1] == null || Rooms[loc.x + 1, loc.y].Type == RoomType.Boss)
             availableDir.Add(new Location(loc.x, loc.y + 1));
+
         if (loc.y - 1 >= 0 && Rooms[loc.x, loc.y - 1] == null && includeBtm)
             availableDir.Add(new Location(loc.x, loc.y - 1));
 
         return availableDir;
+    }
+
+    public RoomType? GetAvailableType()
+    {
+        RoomType? returnedType = null;
+
+        if (RemainingBRewardRooms == 0 && RemainingARewardRooms == 0 && RemainingEnemiesRooms == 0)
+            return returnedType;
+
+        while (returnedType == null)
+        {
+            switch (Random.Range(0, 3))
+            {
+                case 1:
+                    RemainingBRewardRooms--;
+                    returnedType = RoomType.BuffReward;
+                    break;
+                case 2:
+                    RemainingARewardRooms--;
+                    returnedType = RoomType.AbilityReward;
+                    break;
+                case 3:
+                    RemainingEnemiesRooms--;
+                    returnedType = RoomType.Enemies;
+                    break;
+            }
+        }
+
+        return returnedType;
     }
 }
